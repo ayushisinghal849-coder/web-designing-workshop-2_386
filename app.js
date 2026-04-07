8b0ec6de3ccb8e8126697dfecd4d2779dcc76fd1
ayushisinghal849-coder <ayushisinghal849@gmail.com>
2026-04-07 13:35:55 +0530

    app.js done

---
+++ app.js
@@ -0,0 +1,50 @@
+var name="John Doe";
+var age= 30;
+var isemployed= true;
+console.log(name);
+console.log(age);
+console.log(isemployed);
+var age=50;
+console.log(age);
+// let does not allow declaration
+let name="Ayushi Singhal";
+let age= 20;
+let isemployed= true;
+console.log(name);
+console.log(age);
+console.log(isemployed);
+let age=30;
+console.log(age);
+
+if(true){
+    var age=30;
+}
+    console.log(age);
+
+//This will cause an error because age is not defined outside the block
+
+if(true){
+    let age=30;
+}
+    console.log(age); 
+const PI=3.14;
+console.log(PI);
+PI=3.14159;
+console.log(PI);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isemployed);
+let name="Ayushi";
+let age1= 18;
+console.log("my name is " + name + " and my age is " + age1); //Concatenation
+console.log(`My name is ${name} and my age is ${age1}`); 
+boolean
+let data = null;
+console.log(data);
+console.log(typeof data);
+
+//undefined
+let result;
+console.log(result);
+console.log(typeof result);
+
